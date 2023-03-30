9514b37a014edf821cedd3be55476972f8656969
Fudy <104286054+Fudyy@users.noreply.github.com>
2023-03-30 18:12:28 -0300

    Add basic functionality and logger

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 import discord
+from discord.ext import commands
+from utils import logger
+
 import os
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
-client = discord.Client(intents=discord.Intents.default())
+#Configuracion del cliente
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix=".", intents=intents)
 
-client.run(os.environ.get("TOKEN"))
+#Setup al iniciar el bot
+@bot.event
+async def on_ready():
+    logger(f'Aplicacion funcionando con cliente: {bot.user}')
+    try:
+        synced = await bot.tree.sync()
+        logger(f"Se han sincronizado {len(synced)} comandos.")
+    except Exception as e:
+        print(e)
+
+
+bot.run(os.environ.get('TOKEN'))
---
+++ utils.py
@@ -0,0 +1,5 @@
+def logger(text):
+    """
+    Genera un print anteponiendo '[Taberna Hispana]'
+    """
+    print(f'\x1b[0;37;43m[Taberna Hispana]\x1b[0m {text}')
